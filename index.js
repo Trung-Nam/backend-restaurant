b1420c5abeee10151971b98763aee97c15535464
Trung Nam <trungnam.work@gmail.com>
2024-11-03 23:00:08 +0700

    Setup and Creat api

--- index.js
+++ index.js
@@ -1,17 +1,104 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 6001
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // database and collections
+    const menuCollections = client.db("restaurant").collection("menu");
+    const cartCollections = client.db("restaurant").collection("carts");
+
+    // all menu items operations
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollections.find().toArray();
+      res.send(result);
+    })
+
+    // all cart operations
+
+    // Posting cart to db
+    app.post("/carts", async (req, res) => {
+      const cart = req.body;
+      const result = await cartCollections.insertOne(cart);
+      res.send(result);
+    })
+    // Get cart from db
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email };
+      const result = await cartCollections.find(filter).toArray();
+      res.send(result);
+    })
+
+    // Get a specify cart
+    app.get("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await cartCollections.findOne(filter);
+      res.send(result);
+    })
+
+    // Delete
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await cartCollections.deleteOne(filter);
+      res.send(result);
+    })
+
+    // update carts quantity
+    app.put('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const { quantity } = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: {
+          quantity: parseInt(quantity, 10)
+        },
+      };
+
+      const result = await cartCollections.updateOne(filter, updateDoc, options);
+
+      res.send(result);
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
