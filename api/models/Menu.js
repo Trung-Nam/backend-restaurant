b2d401abee7cc543f6b5aa16f89e252727f5d12e
Trung Nam <trungnam.work@gmail.com>
2024-11-04 20:21:16 +0700

    jwt token and update apis

--- api/models/Menu.js
+++ api/models/Menu.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-// Create schema object for menu items
+// create schema object for Menu Items
 const menuSchema = new Schema({
     name: {
         type: String,
-        trim:true,
+        trim: true,
         required: true,
-        minLength: 3,
+        minlength: 3
     },
-    recipe:String,
-    image:String,
-    category:String,
-    price:Number
+    recipe: String,
+    image: String, 
+    category: String,
+    price: Number,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
 })
 
-// Create model
-const Menu = mongoose.model('Menu',menuSchema);
+// create model
+const Menu = mongoose.model("Menu", menuSchema);
 module.exports = Menu;
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 6001;
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
+
 
 // middleware
 app.use(cors());
@@ -18,12 +20,24 @@ mongoose
   )
   .catch((error) => console.log("Error connecting to MongoDB", error));
 
+
+// jwt authentication
+app.post('/jwt', async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '1hr'
+  })
+  res.send({ token });
+})
+
 //   import routes here
 const menuRoutes = require('./api/routes/menuRoutes');
 const cartRoutes = require('./api/routes/cartRoutes');
+const userRoutes = require('./api/routes/userRoutes')
 
-app.use('/menu', menuRoutes);
+app.use('/menu', menuRoutes)
 app.use('/carts', cartRoutes);
+app.use('/users', userRoutes);
 
 
 
