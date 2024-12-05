799b1f3b857fb472c1c9b04edf110a1ab652eca5
Trung Nam <trungnam.work@gmail.com>
2024-12-06 00:59:59 +0700

    update user address and update user by id

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -5,8 +5,10 @@ const userController = require('../controllers/userControllers');
 const verifyToken = require('../middleware/verifyToken')
 const verifyAdmin = require('../middleware/verifyAdmin')
 
-router.get('/',verifyToken, verifyAdmin,  userController.getAllUsers);
+router.get('/', verifyToken, verifyAdmin, userController.getAllUsers);
+router.get('/:email', verifyToken, userController.getUserByEmail);
 router.post('/', userController.createUser);
+router.put('/:id', verifyToken, userController.updateUser);
 router.delete('/:id', verifyToken, verifyAdmin, userController.deleteUser);
 router.get('/admin/:email', verifyToken, userController.getAdmin);
 router.patch('/admin/:id', verifyToken, verifyAdmin, userController.makeAdmin);
